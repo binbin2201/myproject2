739f450c606c34624ea9675a768d7670ddd3013b
Zhao <ninjatuna970@gmail.com>
2022-04-05 00:13:17 +0800

    commit

--- src/main/java/com/bin/ticket/Ticket.java
+++ src/main/java/com/bin/ticket/Ticket.java
@@ -9,33 +9,34 @@ public class Ticket {
         int choice = Integer.parseInt(scanner.next());
         String startStation = null;
         String endStation = null;
-        switch(choice){
-            case 1 :
-                startStation = "Taipei";
-                break;
-            case 2 :
-                startStation = "Taichung";
-                break;
-            case 3 :
-                startStation = "Kaohsiung";
-                break;
-        }
-        System.out.println("Please enter your terminal station : Taipei = 1 ,Taichung = 2, Kaohsiung = 3");
-        choice = Integer.parseInt(scanner.next());
+            switch (choice) {
+                case 1:
+                    startStation = "Taipei";
+                    break;
+                case 2:
+                    startStation = "Taichung";
+                    break;
+                case 3:
+                    startStation = "Kaohsiung";
+                    break;
+            }
+            System.out.println("Please enter your terminal station : Taipei = 1 ,Taichung = 2, Kaohsiung = 3");
+            choice = Integer.parseInt(scanner.next());
 
-        switch(choice){
-            case 1 :
-                endStation = "Taipei";
-                break;
-            case 2 :
-                endStation = "Taichung";
-                break;
-            case 3 :
-                endStation = "Kaohsiung";
-                break;
-        }
+            switch (choice) {
+                case 1:
+                    endStation = "Taipei";
+                    break;
+                case 2:
+                    endStation = "Taichung";
+                    break;
+                case 3:
+                    endStation = "Kaohsiung";
+                    break;
+            }
 
-        System.out.println(startStation+endStation);
+            System.out.println(startStation + endStation);
+        }
     }
-}
+
 
