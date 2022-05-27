7f918c56d24d5740eb35f3828abd79d9e96885b3
Zhao <ninjatuna970@gmail.com>
2022-05-27 12:53:30 +0800

    homework 2

--- src/main/java/com/bin/ui/GuessFrame.java
+++ src/main/java/com/bin/ui/GuessFrame.java
@@ -1,24 +1,33 @@
 package com.bin.ui;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GuessFrame extends JFrame {
+    JTextField number = new JTextField(10);
+    JButton button = new JButton("Guess");
+    JLabel label = new JLabel("Hi");
     public GuessFrame() {
         super();
         setSize(650, 400);
         setLocation(300, 200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JButton button = new JButton("Hi");
-        JLabel label = new JLabel("hahaha");
         setVisible(true);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                label.setText("Hello");
+                int num = Integer.parseInt(number.getText());
+                System.out.println(num);
             }
         });
+        label.setText("number:");
+        setLayout(new FlowLayout());
+        add(label);
+        add(number);
+        add(button);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
