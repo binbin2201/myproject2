16f96c356df261b60614d4217a920746b3e80377
Zhao <ninjatuna970@gmail.com>
2022-06-17 16:30:01 +0800

    java io file read 2

--- src/main/java/com/bin/score/ScoringFile.java
+++ src/main/java/com/bin/score/ScoringFile.java
@@ -1,5 +1,6 @@
 package com.bin.score;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,16 +9,29 @@ public class ScoringFile {
     public static void main(String[] args) {
         try{
             FileReader fileReader = new FileReader("scores.txt");
-            int data = fileReader.read();
+            BufferedReader reader = new BufferedReader(fileReader);
+             String line = reader.readLine();
+            while(line!= null) {
+                //line = reader.readLine();
+                //System.out.println(line);
+                String[] token = line.split(",");
+                String name = token[0];
+                int english = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Student stu = new Student(name,english,math);
+                stu.print();
+                line = reader.readLine();
+            }
+            /*int data = fileReader.read();
             while(data!= -1){
                 System.out.print((char)data);
                 data = fileReader.read();
-            }
+            }*/
         }catch (FileNotFoundException ex){
             System.out.println("Can't read file");
         }catch (IOException e){
             System.out.println("Can't read file");
         }
-        System.out.println("test");
+
     }
 }
